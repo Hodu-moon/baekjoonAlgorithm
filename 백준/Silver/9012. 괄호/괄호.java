efcf37e182ed8b7a0aad8ae4df686582c84932bf
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2023-01-01 19:46:11 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,19 +1,25 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int num = in.nextInt();
+        int num = Integer.parseInt(br.readLine());
+
         for(int i = 0; i < num; i++){
-            if(isVPS(in.next())){
-                System.out.println("YES");
+            if(isVPS(br.readLine())){
+                sb.append("YES").append('\n');
             }else
-                System.out.println("NO");
+                sb.append("NO").append('\n');
         }
+        System.out.println(sb);
     }
     static public boolean isVPS(String s){
 
