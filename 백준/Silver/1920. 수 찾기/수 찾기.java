756d4f800273dd48242f43238fdfcc2ff474392a
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2023-01-26 21:26:56 +0900

    [Silver IV] Title: 수 찾기, Time: 616 ms, Memory: 43584 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -17,7 +17,6 @@ public class Main {
             i++;
         }
 
-
         int M = Integer.parseInt(br.readLine());
         StringTokenizer st3 = new StringTokenizer(br.readLine());
         int[] Marray = new int[M];
@@ -30,13 +29,65 @@ public class Main {
 
         Arrays.sort(Narray);
         for(int j = 0; j < Marray.length; j++){
-            if(Arrays.binarySearch(Narray, Marray[j]) > -1){
-                sb.append(1);
-            }else {
+            if(binarySearch(Narray, Marray[j]) == -1){
                 sb.append(0);
-            }
+            }else
+                sb.append(1);
             sb.append("\n");
         }
         System.out.println(sb.toString());
+
+        // 내가 푼거 나는 이진탐색을 더 자세히 알기 위해 이분탐색을 구현해 보겟다.
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int[] Narray = new int[N];
+//        int i = 0;
+//        while(st.hasMoreTokens()){
+//            Narray[i] = Integer.parseInt(st.nextToken());
+//            i++;
+//        }
+//
+//
+//        int M = Integer.parseInt(br.readLine());
+//        StringTokenizer st3 = new StringTokenizer(br.readLine());
+//        int[] Marray = new int[M];
+//        i = 0;
+//        while(st3.hasMoreTokens()){
+//            Marray[i] = Integer.parseInt(st3.nextToken());
+//            i++;
+//        }
+//        StringBuilder sb = new StringBuilder();
+//
+//        Arrays.sort(Narray);
+//        for(int j = 0; j < Marray.length; j++){
+//            if(Arrays.binarySearch(Narray, Marray[j]) > -1){
+//                sb.append(1);
+//            }else {
+//                sb.append(0);
+//            }
+//            sb.append("\n");
+//        }
+//        System.out.println(sb.toString());
+    }
+
+
+    public static int binarySearch(int[] arr, int key){
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while(lo <= hi){
+            int mid = (lo + hi) / 2;
+
+            if( key <  arr[mid] ){
+                hi = mid -1 ;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            }else {// key == arr[mid]
+                return 1004;
+            }
+        }
+
+        return -1;
     }
 }
