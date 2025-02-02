07ff52b82084e6ec7b99a0532f4645224de2ac54
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-02-02 19:56:01 +0900

    [Silver III] Title: N과 M (2), Time: 188 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,45 +1,62 @@
+
 import java.util.Scanner;
 
 public class Main {
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        visited = new boolean[N];
+        arr = new int[M];
 
-	static int N;
-	static int M;
-	static StringBuilder sb = new StringBuilder();
-	static int[] arr;
-	static boolean[] visited;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-		arr = new int[M];
-		visited = new boolean[N + 1];
-	
-		back(0, 1);
-		
-		System.out.println(sb);
-	}
+        back(0, 0);
 
-	public static void back(int depth , int start) {
-			if(depth == M){
-				for(int a : arr) {
-					sb.append(a).append(" ");
-				}
-				sb.append("\n");
-				
-				return;
-			}
-		
-			for(int i = start; i <= N; i++) {
-				
-				if(visited[i] == false ) {
-					
-					visited[i] = true;
-					arr[depth] = i;
-					back(depth + 1, i + 1);
-					visited[i] = false;
-					
-				}
-				
-			}
-	}
+        System.out.println(sb.toString());
+
+    }
+
+    public static void back(int depth, int start){
+        if(depth == M){
+            for(int x : arr){
+                sb.append(x + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start ; i < N; i++){
+            if(! visited[i] ){
+                visited[i] = true;
+                arr[depth] = i + 1;
+                back(depth + 1, i + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+
+//    public static void back(int depth){
+//        if(depth == M){
+//            for(int i = 0; i < arr.length; i++){
+//                sb.append(arr[i]).append(" ");
+//            }
+//            sb.append("\n");
+//            return;
+//        }
+//
+//        for(int i = 0; i < N; i++){
+//            if(!visited[i]  ){
+//                visited[i] = true;
+//                arr[depth] = i + 1;
+//                back(depth + 1);
+//                visited[i] = false;
+//            }
+//        }
+//
+//    }
 }
