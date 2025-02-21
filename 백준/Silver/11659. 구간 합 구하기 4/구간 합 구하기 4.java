0e6e144ed2ecff0bf39e89f469d1deb7229a9f6c
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-02-21 19:44:37 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1848 ms, Memory: 257876 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,72 +1,32 @@
-import java.io.*;
+
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] arr ;
-    static StringTokenizer st;
-    static BufferedReader br;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    static int[] sumArr;
-
-
-    public static void main(String[] args) throws IOException {
-
-        br = new BufferedReader(new InputStreamReader(System.in));
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N + 1];
-        sumArr = new int[N + 1];
-
-        makeArr();
-        int sum;
-
-        for(int i = 0; i < M; i++){
-            sum = 0;
-
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            // 5 5
-            if(start == end){
-                bw.write(arr[start] + "\n");
-                continue;
-            }
-
-            // start가 첫번째 부터이면
-            if(start == 1){
-                bw.write(sumArr[end] + "\n");
-                continue;
-            }
-
-            
-            int s = sumArr[end] - sumArr[start - 1];
-            bw.write(s + "\n");
-
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N+1];
+        
+        for(int i = 1; i <= N; i++){
+            arr[i] = arr[i - 1] + sc.nextInt(); 
         }
 
-        bw.flush();
 
+        // printArr(arr);
 
+        for(int i = 0; i < M; i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            System.out.println(arr[end] - arr[start - 1]);
+        }
     }
-
-    static void makeArr() throws IOException{
-        st = new StringTokenizer(br.readLine());
-
-        for(int i = 1; i <= N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-
-            if(i == 1)
-                sumArr[i] = arr[i];
-            else
-                sumArr[i] = arr[i] + sumArr[i - 1];
+    
+    static void printArr(int[] arr){
+        for(int  x : arr){
+           System.out.print(x + " ");
         }
     }
 }
