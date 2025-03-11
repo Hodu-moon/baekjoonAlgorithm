2ea4f24461bfd06092cb20b3965a5b2ad6b975c6
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-03-11 23:23:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 292 ms, Memory: 27188 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,96 +1,53 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static int N, M;
+    static List<Integer> lans = new ArrayList<>();
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int K = scanner.nextInt();
-        int N = scanner.nextInt();
-        long []array = new long[K];
-
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
         long max = 0;
-        for(int i = 0; i < K; i++){
-            array[i] = scanner.nextInt();
-            if(max < array[i])
-                max = array[i];
-        }
-        //??
-        max++;
+        for(int i = 0; i < N;i++){
+            int lan = sc.nextInt();
+            lans.add(lan);
 
+            max = Math.max(max, lan);
+        }
+
+        max++;
         long min = 0;
         long mid = 0;
 
         while(min < max){
-            mid = (min + max) / 2 ; //이게 이진탐색이다.
-            long 몫 = 0;
-            for (int i = 0; i < K; i++) {
-                몫 += array[i] / mid;
+
+            mid = (min + max) / 2;
+
+            long count = 0;
+            for(int lan : lans){
+                count += (lan / mid);
             }
-            if( 몫 < N ){
+
+            // 800 400  인데 3개로 자르라함
+            // 800으로 자름 그러면 1임
+            // 부족함 줄여야함
+            if(count < M){
                 max = mid;
-            }else {
-                min = mid + 1; //
+            }else{
+                min = mid + 1;
             }
         }
-        //upper bound 라고 하는데 정말이지 무슨소린지. 모르겠다.
-
-        System.out.println(min -1);
+        System.out.println(min - 1);
 
 
-
-
-        //2 흐음 이진 탐색을 제대로 이해하지 못하고 있따.
-//        while(true) {
-//            몫 = 0;
-//            for (int i = 0; i < K; i++) {
-//                몫 += array[i] / mid;
-//            }
-//            if (몫 == N) {
-//                System.out.println(mid);
-//                return;
-//            } else if (몫 < N) {
-//                max = mid +1;
-//                mid = (mid + min) / 2;
-//
-//            } else if (몫 > N) {
-//                min = mid +1;
-//                mid = (mid + max) / 2;
-//            }
-//        }
-
-
-
-
-        /*
-        Scanner scanner = new Scanner(System.in);
-        int K = scanner.nextInt();
-        int N = scanner.nextInt();
-        long []array = new long[K];
-        long sum = 0;
-
-        for(int i = 0; i < K; i++){
-            array[i] = scanner.nextInt();
-            sum += array[i];
-        }
-
-        sum = sum/K;
-
-        long 몫 = 0;
-
-        for(long i = sum; i > 0; i--){
-            몫 = 0;
-
-            for(int j = 0; j < K; j++){
-                몫 += array[j]/i;
-            }
-
-            if(몫 == N){
-                System.out.println(i);
-                return;
-            }
-
-        }
-           */
 
     }
+
 }
+
+//4 11
+//802
+//743
+//457
+//539
