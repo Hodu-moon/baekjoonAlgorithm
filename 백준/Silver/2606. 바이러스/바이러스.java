c83787d40ad0c0063e6e9f4fb441c6312f0d8d90
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-03-12 23:14:06 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 13368 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,52 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int N, result;
+    static int[][] graph;
 
-    static int graph[][];
-    static int virus;
-    static boolean visited[];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        graph = new int[N+1][N+1];
-        visited = new boolean[N+1];
-
-
-
-        while (M --> 0){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            graph[x][y] = graph[y][x] = 1;
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        graph = new int[N + 1][N + 1];
+        int M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int before = sc.nextInt();
+            int next = sc.nextInt();
+            graph[before][next] = graph[next][before] = 1;
         }
-        checkVirus(1);
-        System.out.println(virus);
+
+        BFS(1);
+        System.out.println(result);
 
     }
+    static void BFS(int start){
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[N + 1];
+        visited[1] = true;
+        queue.offer(start);
 
-    static void checkVirus(int V){
-        visited[V] = true;
-        for(int i = 1; i < graph.length; i++){
-            if(graph[V][i] == 1 && !visited[i] ){
-                    virus++;
-                    checkVirus(i);
+        while(!queue.isEmpty()){
+            int before = queue.poll();
+
+            for(int i = 1; i <= N; i++){
+
+                if(!visited[i] && graph[before][i] == 1){
+                    visited[i] = true;
+                    queue.offer(i);
+                    result++;
+                }
             }
-        }
 
+        }
     }
 
-
-
+    // 7
+    //6
+    //1 2
+    //2 3
+    //1 5
+    //5 2
+    //5 6
+    //4 7
 }
