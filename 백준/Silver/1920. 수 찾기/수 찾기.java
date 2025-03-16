0895724ec005fb7fca1a03db20d8791c3665041f
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-03-16 16:19:04 +0900

    [Silver IV] Title: 수 찾기, Time: 448 ms, Memory: 57424 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,93 +1,61 @@
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] Narray = new int[N];
-        int i = 0;
-        while(st.hasMoreTokens()){
-            Narray[i] = Integer.parseInt(st.nextToken());
-            i++;
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        // 정렬 되어있어야 함
+        Arrays.sort(arr);
+
+        // 2. 찾기
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < M; i++){
+            int target = Integer.parseInt(st.nextToken());
+
+            sb.append(binarySearch(target) ? 1 : 0).append("\n");
         }
 
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer st3 = new StringTokenizer(br.readLine());
-        int[] Marray = new int[M];
-        i = 0;
-        while(st3.hasMoreTokens()){
-            Marray[i] = Integer.parseInt(st3.nextToken());
-            i++;
-        }
-        StringBuilder sb = new StringBuilder();
+        System.out.println(sb);
 
-        Arrays.sort(Narray);
-        for(int j = 0; j < Marray.length; j++){
-            if(binarySearch(Narray, Marray[j]) == -1){
-                sb.append(0);
-            }else
-                sb.append(1);
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
-
-        // 내가 푼거 나는 이진탐색을 더 자세히 알기 위해 이분탐색을 구현해 보겟다.
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        int N = Integer.parseInt(br.readLine());
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-//        int[] Narray = new int[N];
-//        int i = 0;
-//        while(st.hasMoreTokens()){
-//            Narray[i] = Integer.parseInt(st.nextToken());
-//            i++;
-//        }
-//
-//
-//        int M = Integer.parseInt(br.readLine());
-//        StringTokenizer st3 = new StringTokenizer(br.readLine());
-//        int[] Marray = new int[M];
-//        i = 0;
-//        while(st3.hasMoreTokens()){
-//            Marray[i] = Integer.parseInt(st3.nextToken());
-//            i++;
-//        }
-//        StringBuilder sb = new StringBuilder();
-//
-//        Arrays.sort(Narray);
-//        for(int j = 0; j < Marray.length; j++){
-//            if(Arrays.binarySearch(Narray, Marray[j]) > -1){
-//                sb.append(1);
-//            }else {
-//                sb.append(0);
-//            }
-//            sb.append("\n");
-//        }
-//        System.out.println(sb.toString());
     }
 
+    static boolean binarySearch(int target){
 
-    public static int binarySearch(int[] arr, int key){
-        int lo = 0;
-        int hi = arr.length - 1;
+        int start = 0, end = N - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
 
-        while(lo <= hi){
-            int mid = (lo + hi) / 2;
-
-            if( key <  arr[mid] ){
-                hi = mid -1 ;
-            } else if (key > arr[mid]) {
-                lo = mid + 1;
-            }else {// key == arr[mid]
-                return 1004;
+            if(arr[mid] == target){
+                return true;
+            }else if(arr[mid] > target){
+                end = mid - 1;
+            }else{ // arr[mid] < target
+                start = mid + 1;
             }
         }
 
-        return -1;
+        return false;
     }
 }
+
+//5
+//4 1 5 2 3
+//5
+//1 3 7 9 5
+
+//1
+//1
+//0 ...
