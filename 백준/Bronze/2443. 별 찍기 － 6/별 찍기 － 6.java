9c1adb8cfda01b678db4a47554e5137223a4e111
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-03-22 15:48:04 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 100 ms, Memory: 13192 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printStart(N);
+
+        System.out.println(sb);
+    }
+
+    static void printStart(int N) {
+        int start = 0, end = 2 * N - 2;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N * 2 - 1; j++){
+                if(  start <= j && j <= end){
+                    sb.append("*");
+                }else if( j < start){
+                    sb.append(" ");
+                }else if( j > end){
+                    break;
+                }
+
+            }
+            start++;
+            end--;
+            sb.append("\n");
+        }
+    }
+}
