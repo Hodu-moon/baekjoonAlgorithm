09745878678e3a5c7b90c03d6976016d4f008108
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-03-22 15:27:03 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 100 ms, Memory: 13044 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        printStart(N);
+
+        System.out.println(sb);
+    }
+
+    static void printStart(int N) {
+        for(int i = 0; i < N; i++ ){
+            for(int j = 0; j < N - i; j++){
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+    }
+}
