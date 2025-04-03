fad7156eee3ea7f51fdf01476523fc8720859b9b
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-04-03 21:58:21 +0900

    [Gold IV] Title: 스도쿠, Time: 356 ms, Memory: 15344 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -1,85 +1,107 @@
 
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    static int[][] map = new int[9][9];
-    static List<Position> emptyCells = new ArrayList<>();
-    static boolean[][] rowUsed = new boolean[9][10];
-    static boolean[][] colUsed = new boolean[9][10];
-    static boolean[][] squareUsed = new boolean[9][10];
-    static boolean solved = false;
+    static int[][] map;
+    static boolean[][] rowUsed;
+    static boolean[][] colUsed;
+    static boolean[][] blockUsed;
+    static List<Position> positionList = new ArrayList<>();
+
+    static boolean finish;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 9; i++) {
-            String line = sc.nextLine();
-            for (int j = 0; j < 9; j++) {
-                int val = line.charAt(j) - '0';
-                map[i][j] = val;
-                if (val == 0) {
-                    emptyCells.add(new Position(i, j));
-                } else {
-                    rowUsed[i][val] = true;
-                    colUsed[j][val] = true;
-                    squareUsed[getSquareIndex(i, j)][val] = true;
+        map = new int[9][9];
+        rowUsed = new boolean[9][10];
+        colUsed = new boolean[9][10];
+        blockUsed = new boolean[9][10];
+
+        for(int i = 0; i < 9; i++){
+            String input = sc.nextLine();
+            for(int j = 0; j < 9; j++){
+                int num = input.charAt(j) - '0';
+                map[i][j] = num;
+
+                if(num > 0){
+                    rowUsed[i][num] = true;
+                    colUsed[j][num] = true;
+                    blockUsed[getBlockIndex(i, j)][num] = true;
+                }
+
+                if(num == 0){
+                    positionList.add(new Position(i, j));
                 }
             }
         }
-
-        backtrack(0);
+        back(0);
     }
 
-    static void backtrack(int depth) {
-        if (solved) return;
 
-        if (depth == emptyCells.size()) {
-            printMap();
-            solved = true;
+    static void back(int depth){
+
+        if(finish)
+            return;
+
+        if(depth == positionList.size()){
+
+            for(int[] xx : map){
+                for(int x : xx){
+                    System.out.print(x);
+                }
+                System.out.println();
+            }
+
+            finish = true;
             return;
         }
 
-        Position p = emptyCells.get(depth);
-        int x = p.x;
-        int y = p.y;
-        int square = getSquareIndex(x, y);
+        Position position = positionList.get(depth);
 
-        for (int num = 1; num <= 9; num++) {
-            if (!rowUsed[x][num] && !colUsed[y][num] && !squareUsed[square][num]) {
-                map[x][y] = num;
-                rowUsed[x][num] = true;
-                colUsed[y][num] = true;
-                squareUsed[square][num] = true;
+        for(int num = 1; num <= 9; num++){
 
-                backtrack(depth + 1);
+            if(!rowUsed[position.x][num] && !colUsed[position.y][num]
+                    && !blockUsed[getBlockIndex(position.x, position.y)][num]){
 
-                map[x][y] = 0;
-                rowUsed[x][num] = false;
-                colUsed[y][num] = false;
-                squareUsed[square][num] = false;
+
+                rowUsed[position.x][num] =true;
+                colUsed[position.y][num] = true;
+                blockUsed[getBlockIndex(position.x,  position.y)][num] = true;
+
+                map[position.x][position.y] = num;
+                back(depth + 1);
+
+                map[position.x][position.y] = 0;
+                rowUsed[position.x][num] =false;
+                colUsed[position.y][num] = false;
+                blockUsed[getBlockIndex(position.x,  position.y)][num] = false;
+
+
             }
         }
+
+    }
+    static int  getBlockIndex(int x, int y){
+        return ( x / 3 ) * 3 + y / 3;
     }
 
-    static int getSquareIndex(int x, int y) {
-        return (x / 3) * 3 + (y / 3);
-    }
-
-    static void printMap() {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    static class Position {
+    static class Position{
         int x, y;
+
         public Position(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
 }
+//103000509
+//002109400
+//000704000
+//300502006
+//060000050
+//700803004
+//000401000
+//009205800
+//804000107
