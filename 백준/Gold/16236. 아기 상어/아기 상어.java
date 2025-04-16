b0523da7a1341acdc7cb35372591e07d7f5e86f3
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-04-16 09:14:24 +0900

    [Gold III] Title: 아기 상어, Time: 76 ms, Memory: 12208 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -8,133 +8,127 @@ public class Main {
 	static int N, result;
 	static int[][] map;
 	static Shark shark;
-
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-
+	
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-
-		for (int i = 0; i < N; i++) {
+		
+		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < N; j++) {
+			for(int j= 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-
-				if (map[i][j] == 9) {
+				
+				if(map[i][j] == 9) {
 					shark = new Shark(i, j, 2, 0);
 					map[i][j] = 0;
 				}
 			}
 		}
-
+		
+		
 		solve();
-
+		
 		System.out.println(result);
+
 	}
-
+	
 	static void solve() {
-
-		while (true) {
-			Queue<Position> queue = new ArrayDeque<>(); // BFS탐색용
+		
+		while(true) {
 			boolean[][] visited = new boolean[N][N];
-			List<Position> candidates = new ArrayList<>();
-			visited[shark.x][shark.y] = true;
+			Queue<Position> queue = new ArrayDeque<>();
+			
+			Queue<Position> candidates = new PriorityQueue<>();
 			queue.offer(new Position(shark.x, shark.y, 0));
-
+			visited[shark.x][shark.y] = true;
+			
 			int minDistance = Integer.MAX_VALUE;
-
-			while (!queue.isEmpty()) {
-
+			
+			
+			
+			
+			while(!queue.isEmpty()) {
+				
 				Position current = queue.poll();
 				
-				// 현재 위치의 distance 가 최소 거리보다 크다면 가치가 없음
-				// minDistance 가 2였는데 3을 찾는다면 할 필요 없죠
-				if (current.distance > minDistance) {
+				if(current.distance > minDistance) {
 					break;
 				}
-
-				// 물고기 후보에 넣었으면 더이상 BFS 진행하면 안됌
-				if (0 < map[current.x][current.y] && map[current.x][current.y] < shark.level) {
-					candidates.add(current);
+				
+				if(0 < map[current.x][current.y] && map[current.x][current.y] < shark.level ) {
+					candidates.offer(current);
 					minDistance = current.distance;
 					continue;
 				}
-
 				
 				
-				for (int d = 0; d < 4; d++) {
-					int nx = current.x + dx[d];
-					int ny = current.y + dy[d];
-
-					if (!isIn(nx, ny) || visited[nx][ny])
+				for(int i = 0; i < 4; i++) {
+					int nx = current.x + dx[i];
+					int ny = current.y + dy[i];
+					
+					if(!isIn(nx, ny) || visited[nx][ny]) {
 						continue;
-					
-					visited[nx][ny] = true;
-
-					if (map[nx][ny] <= shark.level) {
-						
-						queue.offer(new Position(nx, ny, current.distance + 1));
 					}
-				}
-
-			}
-
 					
-			if (candidates.isEmpty())
-				break;
+					if(map[nx][ny] <= shark.level) {
+						visited[nx][ny] = true;
+						queue.offer(new Position(nx, ny , current.distance + 1));
+					}
+					
+					
+					
+				}
+				
 
-			candidates.sort((p1, p2) -> {
-				if (p1.distance != p2.distance)
-					return p1.distance - p2.distance;
-				else if (p1.x != p2.x)
-					return p1.x - p2.x;
-				else
-					return p1.y - p2.y;
-
-			});
-
-			Position target = candidates.get(0);
-
-			// 샤크 처리
-			if (++shark.totalEat == shark.level) {
-				shark.level++;
-				shark.totalEat = 0;
+				
 			}
+			
+			if(candidates.isEmpty())
+				break;
+			
+			Position target = candidates.poll();
+			
+			map[target.x][target.y] = 0;
+			
+			if(++shark.totalEat == shark.level) {
+				shark.totalEat = 0;
+				shark.level++;
+			}
+			
+			result += target.distance;
+		
 			shark.x = target.x;
 			shark.y = target.y;
-
-			map[target.x][target.y] = 0;
-			// 정답 처리
-			result += target.distance;
 			
 			
-//			System.out.println(shark);
 //			printMap();
-//			System.out.println();
-
+			
 		}
-	}
-
-	static boolean isIn(int x, int y) {
-		return 0 <= x && x < N && 0 <= y && y < N;
+		
 	}
 	
 	static void printMap() {
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
+			for(int j =0; j < N ;j++) {
 				if(i == shark.x && j == shark.y) {
 					System.out.print("* ");
-				}else
+				}else {
 					System.out.print(map[i][j] + " ");
+
+				}
 			}
 			System.out.println();
 		}
 	}
-
-	static class Position {
+	
+	static boolean isIn(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
+	}
+	
+	static class Position implements Comparable<Position>{
 		int x, y, distance;
 
 		public Position(int x, int y, int distance) {
@@ -145,15 +139,21 @@ public class Main {
 		}
 
 		@Override
-		public String toString() {
-			return "Position [x=" + x + ", y=" + y + ", distance=" + distance + "]";
+		public int compareTo(Position o) {
+			// TODO Auto-generated method stub
+			if(distance != o.distance)
+				return distance - o.distance;
+			else if(x != o.x) {
+				return this.x - o.x ;
+			}else {
+				return this.y - o.y ;
+			}
 		}
 		
 		
-
 	}
-
-	static class Shark {
+	
+	static class Shark{
 		int x, y, level, totalEat;
 
 		public Shark(int x, int y, int level, int totalEat) {
@@ -163,14 +163,6 @@ public class Main {
 			this.level = level;
 			this.totalEat = totalEat;
 		}
-
-		@Override
-		public String toString() {
-			return "Shark [x=" + x + ", y=" + y + ", level=" + level + ", totalEat=" + totalEat + "]";
-		}
-
-		
 		
 	}
-
 }
