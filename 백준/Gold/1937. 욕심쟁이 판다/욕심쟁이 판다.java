6e4352340bbf366eccb0d5ad199f088ff51ed86b
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-04-18 11:39:56 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 384 ms, Memory: 40340 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,91 +1,66 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    static int N, resultLength;
-    static int[][] map, result;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+	static int[][] map;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	
+	static int N;
+	static int[][] dp;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+		dp = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int max= 0;
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				max = Math.max(max, DFS(i, j));
+			}
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+		System.out.println(max);
+	}
+	
+	static int DFS(int x, int y) {
+		if(dp[x][y] != 0)
+			return dp[x][y];
+		
+		dp[x][y] = 1;
+		
+		
+		for(int i = 0 ; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(!isIn(nx, ny))
+				continue;
+			
+			if(map[nx][ny] > map[x][y])
+				dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
+		}
+		
+		
+		
+		return dp[x][y];
+		
+	}
+	
+	
+	
+	static boolean isIn(int x, int y) {
+		return 0 <= x && x < N && 0 <= y && y < N;
+	}
+	
 
-        map = new int[N][N];
-        result = new int[N][N];
-        StringTokenizer st ;
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0; j < N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                int currentLength = DFS(i, j);
-                resultLength = Math.max(currentLength, resultLength);
-            }
-        }
-
-        System.out.println(resultLength);
-
-
-    }
-
-    static int DFS(int x, int y){
-
-        if(result[x][y] > 0)
-            return result[x][y];
-
-        int maxLength = 1;
-
-
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(!isIn(nx, ny))
-                continue;
-
-            if(map[x][y] < map[nx][ny]){
-                int next = DFS(nx, ny) + 1;
-
-                if(maxLength < next){
-                    maxLength = next;
-                }
-
-            }
-        }
-
-        return result[x][y] = maxLength;
-
-    }
-
-
-    static class Position{
-        int x, y;
-
-        public Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-
-
-    static boolean isIn(int x, int y){
-        return 0 <= x && x < N && 0 <= y && y < N;
-    }
 }
-
-// N 1< 500
-//4
-//14 9 12 10
-//1 11 5 4
-//7 15 2 13
-//6 3 16 8
