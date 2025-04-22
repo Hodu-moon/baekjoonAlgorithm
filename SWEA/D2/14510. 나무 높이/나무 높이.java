4ab7e8b1fc3d3f68e18105439649261977511848
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-04-22 15:18:34 +0900

    [D2] Title: 나무 높이, Time: 92 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,75 +1,65 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
-    static int N, maxHeight, result;
-    static int[] trees;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for(int t = 1; t <= T; t++){
-            N = Integer.parseInt(br.readLine());
-            trees = new int[N];
-            maxHeight = 0;
-            result = 0;
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	
+	static int N, maxHeight, result;
+	static int[] trees;
+	public static void main(String[] args) throws Exception{
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for(int t = 1; t <= T; t++) {
+			N = Integer.parseInt(br.readLine().trim());
+			maxHeight = Integer.MIN_VALUE;
+			trees = new int[N];
+			
+			st = new StringTokenizer(br.readLine().trim());
+			for(int i = 0; i < N; i++) {
+				trees[i] = Integer.parseInt(st.nextToken());
+				maxHeight = Math.max(maxHeight, trees[i]);
+			}
+			
+			int even = 0;
+			int odd = 0;
+			
+			for(int i = 0; i < N; i++) {
+				int difference = maxHeight - trees[i];
+				
+				if(difference == 0)
+					continue;
+				
+				even += difference / 2;
+				odd += difference % 2;
+			}
+			
+			if(even > odd) {
+				while(Math.abs(even - odd) > 1) {
+					even--;
+					odd += 2;
+				}
+			} 
+			
+			if(even == odd) {
+				result = even + odd;
+			}else if(even < odd) {
+				result = odd * 2 - 1;
+			}else {  // even > odd
+				result = even * 2;
+			}
+			
+			
+			sb.append("#").append(t).append(" ").append(result).append("\n");
+		}
+		System.out.println(sb);
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i = 0; i < N; i++){
-                trees[i] = Integer.parseInt(st.nextToken());
-                maxHeight = Math.max(maxHeight, trees[i]);
-            }
-
-            solve();
-
-
-            sb.append("#").append(t).append(" ").append(result).append("\n");
-        }
-
-        System.out.println(sb);
-
-    }
-
-    static void solve(){
-
-        int even = 0;
-        int odd = 0;
-
-        for(int i = 0; i < N; i++){
-            int difference =  maxHeight - trees[i];
-
-            if(difference == 0)
-                continue;
-
-            even += difference / 2;
-            odd += difference % 2;
-
-        }
-
-        if(odd < even){
-            while(Math.abs(even - odd)  > 1){
-                even--;
-                odd += 2;
-            }
-        }
-
-        // 계산 시작
-        if(odd > even){
-            result = odd * 2 - 1;
-        }else if(odd < even){
-            result = even * 2;
-        }else { // odd == even
-            result = even + odd;
-        }
-
-
-
-    }
+	}
+	
+	
 
 }
 
-//50
-//2
-//5 5
