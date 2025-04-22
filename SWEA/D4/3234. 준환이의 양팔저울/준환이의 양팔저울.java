542ee132e5d7b1ce59ab6fb5ca54cb709222fadf
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-04-22 14:48:27 +0900

    [D4] Title: 준환이의 양팔저울, Time: 940 ms, Memory: 27,132 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -1,73 +1,95 @@
-
-
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-    static int N;
-    static  int T;
-    static int[] arr;
+	
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static int N, result;
+	static int[] weights;
+	static boolean[] visited;
+	static int[] selected;
 
-    static int[] temp;
-    static boolean[] visited;
-    static int result = 0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        T = sc.nextInt();
-        for(int t = 1; t <= T; t++){
-            N = sc.nextInt();
-            arr = new int[N];
-            temp = new int[N];
-            visited = new boolean[N];
-            result = 0;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for(int t = 1 ; t <= T; t++) {
+			N = Integer.parseInt(br.readLine().trim());
+			weights = new int[N];
+			result = 0;
+			
+			st = new StringTokenizer(br.readLine().trim());
+			for(int i = 0; i < N; i++) {
+				weights[i] = Integer.parseInt(st.nextToken());
+			}
+		
+			// 1. 순열을 짬.
+			// 2. 그 수열로 백트레킹 
+			visited = new boolean[N];
+			selected = new int[N];
+			permutation(0);
+			
+			sb.append("#").append(t).append(" ").append(result).append("\n");
+		}
+		
+		
+		System.out.println(sb);
+		
+	}
+	
+	
+	static void permutation(int depth) {
+		
+		if(depth == N) {
+			// 여기서 처리 
+			solve(0, 0, 0);
+			return;
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(visited[i])
+				continue;
+			
+			visited[i] = true;
+			selected[depth] = i;
+			permutation(depth + 1);
+			visited[i] = false;	
+		}
+		
+	}
+	
+	static void solve(int depth, int leftWeight, int rightWeight) {
+		// back tracking 조건
+		if(rightWeight > leftWeight) {
+			return;
+		}
+		
+		
+		// 기저조건
+		if(depth == N) {
+			result++;
+			return;
+		}
+		
+		// left에 올리는거
+		solve(depth + 1, leftWeight  + weights[selected[depth]], rightWeight);
+		// right에 올리는거 
+		solve(depth + 1, leftWeight, rightWeight + weights[selected[depth]]);
+		
+	}
 
-            for(int i = 0; i < N; i++){
-                arr[i] = sc.nextInt();
-            }
-            Arrays.sort(arr);
-            back(0);
-
-            System.out.println( "#" + t + " "+ result);
-
-        }
-    }
-    static void back(int depth){ // 순열 뽑기
-        // 기저조건
-        if(depth == N){
-            scale(0, 0, 0);
-            return;
-        }
-
-        for(int i = 0; i < N; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                temp[depth] = arr[i];
-                back(depth + 1);
-                visited[i] =false;
-            }
-        }
-    }
-
-    static void scale(int depth, int left, int right){
-        // 기저조건
-        if(left < right){
-            return;
-        }
-        if(depth == N){
-            result++;
-            return;
-        }
-
-        scale(depth + 1, left  + temp[depth], right); // 왼쪽에 하나 올리는거
-        scale(depth + 1, left, right + temp[depth]);
-
-    }
 }
 
 //
 //3
-// 3
-//        1 2 4
-//        3
-//        1 2 3
-//        9
-//        1 2 3 5 6 4 7 8 9
+//3
+//1 2 4
+//3
+//1 2 3
+//9
+//1 2 3 5 6 4 7 8 9
