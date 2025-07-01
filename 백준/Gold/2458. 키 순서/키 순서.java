dec53415efaf03f13ff3b2182ae4c71d197ec79c
bbangho <82320750+Hodu-moon@users.noreply.github.com>
2025-07-02 00:02:04 +0900

    [Gold IV] Title: 키 순서, Time: 880 ms, Memory: 154272 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,86 +1,52 @@
-
 import java.util.*;
 
-//
 public class Main {
+    static final int MAX_N = 500;
+    static boolean[][] adj;
+    static int N, M;
 
-    static int N, M, count, result;
-    static int[][] graph;
-    static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         M = sc.nextInt();
 
-        graph = new int[N+1][N+1];
+        adj = new boolean[N + 1][N + 1];
 
-        for(int i = 0; i < M; i++){
-            int before = sc.nextInt();
-            int next = sc.nextInt();
-
-            graph[before][next] = 1;
+        // 입력 처리
+        for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            adj[a][b] = true;
         }
 
+        // 플로이드-워셜
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (adj[i][k] && adj[k][j]) {
+                        adj[i][j] = true;
+                    }
+                }
+            }
+        }
 
-        for(int i = 1; i <= N; i++){
-            count = 1;
-            visited = new boolean[N + 1];
-            solve(i);
+        // 각 학생이 자신보다 작은 학생 수, 큰 학생 수
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            int smallCount = 0;
+            int bigCount = 0;
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (adj[i][j]) bigCount++;    // i < j
+                if (adj[j][i]) smallCount++; // j < i
+            }
+
+            if (smallCount + bigCount == N - 1) {
+                result++;
+            }
         }
 
         System.out.println(result);
-
-    }
-
-    static void solve(int x){
-
-        Queue<Integer> queue = new ArrayDeque<>();
-        visited[x] = true;
-
-        // 위로 아는 애들
-        queue.offer(x);
-
-        while(!queue.isEmpty()){
-            int before = queue.poll();
-
-            for(int i = 1; i <= N; i++){
-
-                if(!visited[i] && graph[i][before] == 1){
-                    visited[i] = true;
-                    queue.offer(i);
-                    count++;
-                }
-
-            }
-        }
-
-        queue = new ArrayDeque<>();
-        queue.offer(x);
-
-        while(!queue.isEmpty()){
-            int bofore = queue.poll();
-
-            for(int i = 1; i <= N; i++){
-                if(!visited[i] && graph[bofore][i] == 1){
-                    queue.offer(i);
-                    count++;
-                    visited[i] = true;
-
-
-                }
-            }
-        }
-
-        if(count == N){
-            result++;
-        }
-
     }
 }
-//6 6
-//1 5
-//3 4
-//5 4
-//4 2
-//4 6
-//5 2
